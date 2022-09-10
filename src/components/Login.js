e2a5e322f4f0f6730593c600a1eb2588f349d951
mahesusPrajapati <mahesusp@gmail.com>
2022-09-10 22:29:15 +0530

    commit

---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+
+const Login = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="bg-light border">
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="with a placeholder"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="password placeholder"
+                  type="password"
+                />
+              </FormGroup>
+              <Button>Submit</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
